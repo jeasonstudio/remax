71d7a6adea6544eeab6e49533b82dcb52bf7c700
Jeason <me@cowpoke.cc>
2023-03-22 11:22:17 +0800

    fix: vercel config

--- scripts/serve.mjs
+++ scripts/serve.mjs
@@ -5,17 +5,18 @@ import 'zx/globals';
 import handler from 'serve-handler';
 import http from 'http';
 
-const { outputDirectory, ...config } = require('../vercel.json');
+const config = require('../vercel.json');
 
 const server = http.createServer((request, response) => {
   // You pass two more arguments for config and middleware
   // More details here: https://github.com/vercel/serve-handler#options
   return handler(request, response, {
-    ...config,
-    public: outputDirectory,
+    public: config.outputDirectory,
+    etag: true,
     // `cleanUrls` is false when production,
     // here we override it to make serve support historyApiFallback
     cleanUrls: ['!**/extensions/**/*', '!**/out/**/*'],
+    rewrites: [{ source: '/p/*', destination: '/index.html' }],
   });
 });
 
